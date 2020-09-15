40ec1f6d8bbb474240cfbd8a6fce59d551a0e845
gianca243 <gianca243@gmail.com>
2020-09-15 12:33:11 -0500

    first commit

---
+++ ejemplo.js
@@ -0,0 +1,8 @@
+
+for(let i=0 ; i<101 ; i++){
+  document.write("<div class='bor'>")
+  for(let j=0 ; j<11 ; j++){
+    document.write(i,"x",j,"= ",i*j+" - ")
+  }
+  document.write("</div><br>")
+}
